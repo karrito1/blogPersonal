0288d5ce56035582f0e3b22352927490f7c6e04e
Cristian Camilo Peña Villa <kamilovilla554@gmail.com>
2026-04-30 11:02:00 -0500

    cambio de conexion de base de datos para clever Cl

--- backend/src/models/baseDatos.js
+++ backend/src/models/baseDatos.js
@@ -5,9 +5,10 @@ let conx;
 
 try {
   conx = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    database: "empresaproductos",
+    host: "bidvu8ffo1u6xwbpb9th-mysql.services.clever-cloud.com",
+    user: "uib2hj8nfina7f2r",
+    password: "tA0d4u9h87iQvTWI0x6E",
+    database: "bidvu8ffo1u6xwbpb9th",
     port: "3306",
     waitForConnections: true,
     connectionLimit: 10,
